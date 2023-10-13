62a04e5e68bea7b54ab905828d62c2d5b7436379
Iris Sanders <irissanders@live.nl>
2023-10-13 16:43:00 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: irsander <irsander@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:36:03 by irsander          #+#    #+#             */
-/*   Updated: 2023/10/10 16:57:37 by irsander         ###   ########.fr       */
+/*   Updated: 2023/10/13 16:42:46 by irsander         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,23 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
+	int		needle_len;
 
 	i = 0;
+	needle_len = ft_strlen(needle);
 	if (needle[0] == '\0')
 		return ((char *)haystack);
-	while (haystack[i])
+	while (haystack[i] && i < len && (needle_len + i <= len))
 	{
-		if (ft_strncmp(&haystack[i], needle, len) == 0)
+		if (ft_strncmp(&haystack[i], needle, needle_len) == 0)
 			return ((char *)&haystack[i]);
 		i++;
 	}
 	return (NULL);
 }
 
+// while (haystack[i] && i < len && (needle_len + i <= len)) 
+// laatste voor overflow
 // int main(void)
 // {
 //     const char *haystack;
